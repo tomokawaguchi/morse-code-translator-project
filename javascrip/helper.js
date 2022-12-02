775d0bcbbbda7f06597f984d3b58a9fd0fcd3c6a
Tomoko Kawaguchi <tomo.k.dev@gmail.com>
2022-12-02 20:55:09 +1100

    Pushing some progress before writing some test cases

--- javascrip/helper.js
+++ javascrip/helper.js
@@ -1,31 +1,40 @@
 import { morseCode } from "./data.js";
 
+const invalidChar = "\uFFFD";
+const emptySpace = "\u00A0";
+
 // Handle translation english to morse code
 export const convertToMorse = (str) => {
-	return str
+	const morseArrWithSpace = str
 		.toUpperCase()
 		.split("")
 		.map((el) => {
-			return morseCode[el] ? morseCode[el] : el;
-		})
-		.join(" ");
+			if (el.toLowerCase() == "-" || el.toLowerCase() == ".") return invalidChar;
+			if (el === " ") return emptySpace;
+			if (morseCode[el]) return morseCode[el];
+			return invalidChar;
+		});
+	return morseArrWithSpace.join(" ");
 };
 
-// Handle translation morse code english
+// Handle translation morse code to english
 const entriesArr = Object.entries(morseCode);
+const isInList = (value) => {
+	const element = entriesArr.filter((entry) => {
+		if (entry[1] === value) {
+			return entry[0];
+		}
+	});
+	return element.length > 0 ? element[0][0] : undefined;
+};
 
 export const convertToEnglish = (str) => {
-	const charArr = str
-		.split(" ")
-		.map((el) => {
-			return entriesArr.filter((each) => {
-				if (each[1] === el) {
-					return each[0];
-				}
-			});
-		})
-		.map((each) => each[0][0].toLowerCase())
-		.join("");
-	console.log(charArr);
-	return charArr;
+	const sortedArr = [];
+	const charArr = str.split(" ").map((each) => {
+		if (isInList(each)) return isInList(each).toLowerCase();
+		if (each === " ") return emptySpace;
+		return invalidChar;
+	});
+	console.log({ charArr });
+	return charArr.join(" ");
 };
